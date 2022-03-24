3bcc28e7743b37a9dd6dcecdc2fcca96c8a40326
mikelin <mikelin@wondercore.com>
2022-03-24 16:56:23 +0800

    write some code

---
+++ TestLibrary/Classes/TestLibrary.swift
@@ -0,0 +1,46 @@
+//
+//  TestLibrary.swift
+//  TestLibrary_Example
+//
+//  Created by wondercise on 2022/3/24.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import FitnessDevice
+import RxSwift
+
+open class LibraryTest1{
+    
+    open func showTest1() -> Single<Int>{
+        return Single<Int>.create(subscribe: { obs in
+            obs(.success(500))
+            return Disposables.create()
+        })
+    }
+    
+    public func showTest2() -> Single<Int>{
+        return Single<Int>.create(subscribe: { obs in
+            obs(.success(400))
+            return Disposables.create()
+        })
+    }
+}
+
+
+public class LibraryTest2{
+    
+    open func showTest1() -> Single<Int>{
+        return Single<Int>.create(subscribe: { obs in
+            obs(.success(300))
+            return Disposables.create()
+        })
+    }
+    
+    public func showTest2() -> Single<Int>{
+        return Single<Int>.create(subscribe: { obs in
+            obs(.success(200))
+            return Disposables.create()
+        })
+    }
+}
